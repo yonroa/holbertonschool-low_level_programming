6e0e2c2135ce4e552e1f7cd631f70c8b6d2cb13c
yonroa <yonanderson11@gmail.com>
2022-02-10 10:04:18 -0800

    Printing with print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print the message between parentheses
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
