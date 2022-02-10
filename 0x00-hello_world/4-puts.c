196c5ea245b63b0b4e0ef3d635c9f3e2e7e4acc7
yonroa <yonanderson11@gmail.com>
2022-02-10 09:57:08 -0800

    First message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: 0
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
