b8642b2fbb455c0ab35ac3d781a2127130d80062
yonroa <yonanderson11@gmail.com>
2022-02-11 12:03:04 -0800

    Update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet except q and e
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	cha = 97;
+
+	while (97 < 123)
+	{
+		if ((cha != 101) && (cha != 113))
+		{
+			putchar(cha);
+			cha++;
+		}
+		else
+		{
+			cha++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
