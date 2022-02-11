c9528ef4aad5762d2da7be0e12c6617481440ffa
yonroa <yonanderson11@gmail.com>
2022-02-11 14:30:46 -0800

    Update

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - print the combination of differents numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int u;
+
+	for (n=48; i <= 56; n++)
+	{
+		for (u=49; u <= 57; u++)
+		{
+			if (u > n)
+			{
+				putchar(n);
+				putchar(u);
+				if (n != 56 || u != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
