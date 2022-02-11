2fa86975bc4432003ec3d19282fd5add7debb01c
yonroa <yonanderson11@gmail.com>
2022-02-11 08:20:47 -0800

    Update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	car = 97
+	while (car < 123)
+	{
+		putchar(car)
+		car++;
+	}
+	return (0);
+}
