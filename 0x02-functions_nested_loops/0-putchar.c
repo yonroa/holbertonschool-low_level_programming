b28e5763bdc14b0bde9d7fcccfabeb8e259ce436
yonroa <yonanderson11@gmail.com>
2022-02-14 08:04:09 -0800

    First

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print '_putchar'
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	putchar(char _);
+	putchar(char p);
+	putchar(char u);
+	putchar(char t);
+	putchar(char c);
+	putchar(char h);
+	putchar(char a);
+	putchar(char r);
+	putchar('\n');
+	return (0);
+}
