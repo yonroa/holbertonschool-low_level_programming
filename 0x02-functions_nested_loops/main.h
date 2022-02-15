1e90bc82071392b520156c0e78c9c0aaec61c501
yonroa <yonanderson11@gmail.com>
2022-02-15 06:58:09 -0800

    Update

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,25 +4,3 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-
-void print_alphabet(void)
-{
-	int n = 97;
-
-	while (n < 123)
-	{
-		_putchar(n);
-		n++;
-	}
-}
-
-void print_alphabet_x10(void)
-{
-	int u = 0;
-
-	while (u <= 10)
-	{
-		print_alphabet;
-		u++;
-	}
-}
