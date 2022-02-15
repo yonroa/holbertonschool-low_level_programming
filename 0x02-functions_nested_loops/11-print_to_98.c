e7428b7eb7ec95ffe6ef57aebe2544576e8985e4
yonroa <yonanderson11@gmail.com>
2022-02-15 12:39:22 -0800

    Update

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - print the natural numbers until 98
+ * @n: Start number
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		int u = 98;
+
+		while (n >= 98)
+		{
+			printf(n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+		putchar('\n');
+	}
+	else if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf(n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+		putchar('\n');
+	}
+	else
+	{
+		printf(98);
+	}
+	putchar('\n');
+}
