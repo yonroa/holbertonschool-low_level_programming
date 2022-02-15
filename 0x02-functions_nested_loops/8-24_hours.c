2031769f05913efbc0a69988f757437c1ae1d914
yonroa <yonanderson11@gmail.com>
2022-02-15 09:53:04 -0800

    Update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,13 +5,18 @@
  */
 void jack_bauer(void)
 {
-	for (int n1 = 48; n <= 50; n1++)
+	int n1;
+	int n2;
+	int n3;
+	int n4;
+
+	for (int n1 = 48; n1 <= 50; n1++)
 	{
-		for (int n2 = 48; n <= 51; n2++)
+		for (int n2 = 48; n2 <= 51; n2++)
 		{
-			for (int n3 = 48; n <= 53; n3++)
+			for (int n3 = 48; n3 <= 53; n3++)
 			{
-				for (int n4 = 48; n <= 57; n4++)
+				for (int n4 = 48; n4 <= 57; n4++)
 				{
 					_putchar(n1);
 					_putchar(n2);
