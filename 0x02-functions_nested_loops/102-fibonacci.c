3e658bd281010a8de8f7ca7d16c309fe576dc7b9
yonroa <yonanderson11@gmail.com>
2022-02-15 15:08:04 -0800

    Update

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the fibonacci numbers until the 50
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int r = 1;
+	long int n1 = 1;
+	long int n2 = 2;
+	long int n3;
+
+	while (r <= 50)
+	{
+		if (r != 50)
+		{
+			printf("%li", n1);
+		}
+		else
+		{
+			printf("%li\n", n1);
+		}
+		n3 = n1 + n2;
+		n1 = n2;
+		n2 = n3;
+	}
+	return (0);
+}
