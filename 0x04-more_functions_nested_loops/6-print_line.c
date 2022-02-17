bae70ac1dad73f412b978cd4048bcbd39b778861
yonroa <yonanderson11@gmail.com>
2022-02-17 09:01:23 -0800

    Update

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - print a line in the terminal
+ * @n: Lenght of the line
+ */
+
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		_putchar(95)*n;
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
