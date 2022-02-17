6e9f4cbd28b63c05549f8dbd2f34651bc7595756
yonroa <yonanderson11@gmail.com>
2022-02-17 10:33:05 -0800

    Update

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,7 +12,7 @@ void print_diagonal(int n)
 		int i;
 		int u;
 
-		for (i = 0; i <= n; i++)
+		for (i = 1; i <= n; i++)
 		{
 			for (u = 0; u <= i; u++)
 			{
--- 0x04-more_functions_nested_loops/pruebas.c
+++ 0x04-more_functions_nested_loops/pruebas.c
@@ -3,6 +3,7 @@
 int main(void)
 {
 	print_diagonal(10);
+	print_diagonal(3);
 	print_diagonal(-4);
 	return (0);
 }
