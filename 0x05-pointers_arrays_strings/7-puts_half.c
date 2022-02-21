17283fbf589ca6124c781a6a4dd03613e03db154
yonroa <yonanderson11@gmail.com>
2022-02-21 16:12:46 -0500

    Update

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,7 +15,7 @@ void puts_half(char *str)
 
 	if (i % 2 == 0)
 	{
-		int u = 1;
+		int u = 0;
 
 		while (u <= i - 1)
 		{
@@ -28,7 +28,8 @@ void puts_half(char *str)
 	}
 	else
 	{
-		int u = 1;
+		int u = 0;
+		i = i - 1;
 
 		while (u <= i)
 		{
