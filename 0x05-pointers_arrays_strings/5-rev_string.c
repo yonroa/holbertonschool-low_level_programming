3d68b53eec853d9a39b366a115ce8443e5d6b647
yonroa <yonanderson11@gmail.com>
2022-02-21 14:42:59 -0500

    Update

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,12 +13,13 @@ void rev_string(char *s)
 		i++;
 	}
 
-	int tmp, u;
+	int tmp;
+	int u;
 
 	for (u = 0; u < i / 2; i++)
 	{
 		tmp = s[u];
 		s[u] = s[i - u -1];
-		s[i -x -1] = tmp;
+		s[i -u -1] = tmp;
 	}
 }
