8f8fc30df7b1b6e50ec25180e681c45cc71fa9aa
yonroa <yonanderson11@gmail.com>
2022-02-21 12:25:32 -0500

    Update

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - Print an string
+ * @str: String to be printed
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+}
