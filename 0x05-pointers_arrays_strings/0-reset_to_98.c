7b4053234c7504cfd8da92bc0dafad8fab327711
yonroa <yonanderson11@gmail.com>
2022-02-21 11:21:00 -0500

    Update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,8 +3,6 @@
 /**
  * reset_to_98 - Update n to point to 98
  * @n: Pointer to be updated
- *
- * Return: n
  */
 void reset_to_98(int *n)
 {
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * swap_int - Swap two integers
+ * @a: First integer
+ * @b: Second integer
+ */
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
