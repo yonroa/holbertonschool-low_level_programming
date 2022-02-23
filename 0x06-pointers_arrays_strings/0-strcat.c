de1cbff1a6329fae0dfbfc9a69379c0958b2abc1
yonroa <yonanderson11@gmail.com>
2022-02-23 15:31:22 -0500

    Update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,6 +2,23 @@
 
 char *_strcat(char *dest, char *src)
 {
-	dest = dest + src;
+	int i = 0;
+	int u = 0;
+	int j;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[u] != '\0')
+	{
+		u++;
+	}
+	for (j = 0; j <= u; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+
 	return (dest);
 }
