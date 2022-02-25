d9aec622342833673ddbb8192afd2f38ec9627a5
yonroa <yonanderson11@gmail.com>
2022-02-25 10:49:36 -0500

    Update

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+void print_number(int n)
+{
+	unsigned int u;
+
+	if (n < 0)
+	{
+		u = n * -1;
+		_putchar('-');
+	}
+	else
+	{
+		u = n;
+	}
+	if (u / 10)
+	{
+		print_number(u / 10);
+	}
+	_putchar((u % 10) + 48);
+}
