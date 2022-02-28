e1ab38e8f73c85bdea64aa00fcf5e96bdaa82b65
yonroa <yonanderson11@gmail.com>
2022-02-28 18:28:52 -0500

    Update

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * *_strstr - Locates a substring
+ * @haystack: String to be checked
+ * @needle: Bytes to be compared
+ *
+ * Return: A pointer
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j = 0;
+	int h = 0;
+	int k = 0;
+
+	while (haystack[h] != '\0')
+		h++;
+	while (needle[k] != '\0')
+		k++;
+	for (i = 0; i <= h; i++)
+	{
+		for (j = 0; j < k; j++)
+		{
+			if (haystack[i] == needle[j] && haystack[i + 1] == needle[j+1])
+			{
+				return (&haystack[i]);
+			}
+		}
+	}
+	return (NULL);
+}
