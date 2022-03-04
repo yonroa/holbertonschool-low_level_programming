9761eedc5a3611433e973d339fad800b73251a21
yonroa <yonanderson11@gmail.com>
2022-03-04 15:02:52 -0500

    Update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -28,7 +28,15 @@ int main(int argc, char *argv[])
 			}
 			else
 			{
-				sum += atoi(argv[i]);
+				if (atoi(argv[i]) < 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+				else
+				{
+					sum += atoi(argv[i]);
+				}
 			}
 		}
 		printf("%i\n", sum);
