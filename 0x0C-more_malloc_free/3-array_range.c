8368adabe19a94c910f4aa0d1a3ca7e744f98b6f
yonroa <yonanderson11@gmail.com>
2022-03-11 15:48:01 -0500

    Update

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	str = malloc(sizeof(int) * (max - min));
+	str = malloc(sizeof(int) * ((max - min) + 1));
 
 	while (min < max)
 	{
