67a7c59d2ee4ae02adecab07b5de05fe53bb4b48
yonroa <yonanderson11@gmail.com>
2022-03-11 15:43:34 -0500

    Update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,12 +15,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	str = malloc(nmemb * sizeof(size));
+	str = malloc(nmemb * size);
 
 	if (str == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 		*(str + i) = 0;
 
 	return (str);
