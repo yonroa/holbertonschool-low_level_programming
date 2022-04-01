c84e10ca88112555eabe42a878ce0c0462f86d4f
yonroa <yonanderson11@gmail.com>
2022-04-01 10:47:31 -0500

    Fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
-	while (copy)
+	while (copy && (index < idx))
 	{
 		if (idx == 0)
 		{
