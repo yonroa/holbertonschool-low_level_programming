cf2f39fcad7dfa53e203ceed9e1e93d998d08873
yonroa <yonanderson11@gmail.com>
2022-04-04 15:57:17 -0500

    Update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,11 +10,10 @@ unsigned int binary_to_uint(const char *b)
 {
 	int i;
 	unsigned int UnsInt = 0;
-	int len = strlen(b);
 
 	if (!b)
 		return (0);
-	for (i = 0; i < len; i++)
+	for (i = 0; b[i]; i++)
 	{
 		if (b[i] != '1' && b[i] != '0')
 			return (0);
