8561aebe3b9f4af25e596c2344383c82639c8d19
yonroa <yonanderson11@gmail.com>
2022-04-06 14:21:51 -0500

    Upload project

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,5 +18,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	num = write(file, text_content, strlen(text_content));
 	if (num == -1)
 		return (-1);
+	close(file);
 	return (1);
 }
