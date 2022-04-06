765665565f3a3b6178ed60a2225dec145b3a3958
yonroa <yonanderson11@gmail.com>
2022-04-06 14:18:36 -0500

    Upload project

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content)
 	{
 		num = write(file, text_content, strlen(text_content));
-		if (num == -1 || num != strlen(text_content))
+		if (num == -1)
 			return (-1);
 	}
 	return (1);
