1a6d1c4fa0447998dbd4c5b6bc3febf56c768404
yonroa <yonanderson11@gmail.com>
2022-08-10 13:30:04 -0500

    Upload task 100

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,46 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_search - searches for a value in a sorted array of integers
+ * using the Jump search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: the index where value is located, -1 if is not present
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	int jump = sqrt(size), prev = 0, int_size = size;
+
+	while (array[min(jump, size) - 1] < value)
+	{
+		prev = jump;
+		jump += sqrt(value);
+		if (prev >= int_size)
+			return (-1);
+	}
+	while (array[prev] < value)
+	{
+		prev++;
+		if (prev == min(jump, size))
+			return (-1);
+	}
+	if (array[prev] == value)
+		return (prev);
+	return (-1);
+}
+
+/**
+ * min - search the lowest value between two numbers
+ * @a: first number
+ * @b: second number
+ * Return: The lowest number
+ */
+int min(int a, int b)
+{
+	if (b > a)
+		return (a);
+	else
+		return (b);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,4 +6,6 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, int init, int end);
+int jump_search(int *array, size_t size, int value);
+int min(int a, int b);
 #endif
