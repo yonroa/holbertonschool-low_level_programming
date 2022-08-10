7bf2db1b58ad0542428ae9b2e865ad5a69d7343f
yonroa <yonanderson11@gmail.com>
2022-08-10 13:30:39 -0500

    Fiz task 100

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,6 +13,8 @@ int jump_search(int *array, size_t size, int value)
 {
 	int jump = sqrt(size), prev = 0, int_size = size;
 
+	if (!array)
+		return (-1);
 	while (array[min(jump, size) - 1] < value)
 	{
 		prev = jump;
